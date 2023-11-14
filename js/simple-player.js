885332236ad58ff78e4919304cd2c6e5502bbb35
tiyapatel12 <147492019+tiyapatel12@users.noreply.github.com>
2023-11-14 14:10:52 +0000

    audio 1 finished

--- js/simple-player.js
+++ js/simple-player.js
@@ -1,4 +1,79 @@
+// audio control objects
+const audio = new Audio("audio/queencard.mp3");
+const playPauseButton = document.getElementById("play-pause-button");
 
+// display info objects
+const totalTime = document.getElementById("total-time");
+const trackTime = document.getElementById("current-time");
+const seekBar = document.getElementById("seek-bar");
+
+// Store whether the user is currently dragging the seek bar
+let seeking = false;
+
+//things to happen at audio load
+audio.onloadeddata = (event) => {
+    totalTime.innerHTML = formatTime(audio.duration);
+    seekBar.max = Math.floor(audio.duration);
+}
+
+// things to happen as a live event
+audio.ontimeupdate = (event) => {
+    trackTime.innerHTML = formatTime(audio.currentTime);
+    seekBar.value = Math.floor(audio.currentTime);
+    // Only update seekbar when user is not dragging seek bar knob
+  if (!seeking) {
+    seekBar.value = Math.floor(audio.currentTime);
+}
+}
+
+// Detect when audio finishes playing and restart all necessary values
+audio.onended = (event) => {
+    button.src = "images/play.svg";
+    trackTime.innerHTML = formatTime(0);
+    seekBar.value = 0;
+}
+
+
+// if the audio is paused then button will make it play, but if it is playing then the audio will pause
+playPauseButton.onclick = (event) => {
+    if(audio.paused) {
+        audio.play();
+    }
+    else{
+        audio.pause();
+    }
+}
+
+// audio.onplay is an event that is triggered every time the audio starts to play
+audio.onplay = (event) => {
+    // change the button image
+    playPauseButton.src = "images/pause.svg";
+}
+
+// audio.onpause is an event that is triggered every time the audio starts to pause
+audio.onpause = (event) => {
+    playPauseButton.src = "images/play.svg";
+}
+
+// On seekBar input (i.e. when user clicks on the seek bar knob), set seeking to true
+seekBar.oninput = (event) => {
+    seeking = true;
+  };
+  
+  // On seekBar change (i.e., when the user releases the knob to set a new point in the track), update audio player and set seeking to false
+  seekBar.onchange = (event) => {
+    
+    // Update the audio current time to match the seekBar value that the user has set
+    audio.currentTime = seekBar.value;
+    
+    // If the audio is not currently playing, play it
+    if (!audio.paused) {
+      audio.play();
+    }
+  
+    // Set seeking to false
+    seeking = false;
+  };
 
 /**
  * This formatTime function will format time from seconds to a human readable time.
